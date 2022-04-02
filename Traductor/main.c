d710103c829d3506b8cf923ae266cdce3bec9871
Facundo Errobidart <facundoerrobidart2001@gmail.com>
2022-04-02 14:32:27 -0300

    añadimos funciones para parsear operandos en hexa/octal/decimal y para determinar tipo de operador

--- Traductor/main.c
+++ Traductor/main.c
@@ -19,22 +19,25 @@ const char *noOp[] = {"stop"};
 
 int main(int argc, char *argv[])
 {
-    printf("%2X\n",traduceMnemonico("MUL"));
-    printf("%2X\n",traduceMnemonico("JNN"));
-    printf("%2X\n",traduceMnemonico("stop"));
+    printf("%d", parserNumeros("‘A"));
     return 0;
 }
 
+/*ATENCIÓN: C maneja los int automáticamente,
+solamente hay que tener cuidado de poner el formato
+adecuado cuando se hace printf y scanf*/
+
 void leerArchivo(int instructtiones[]){
-    FILE * arch = fopen("Fibonacci.txt", "r");
+    FILE * arch = fopen("Fibonacci.txt", 'r');
     char linea[100];
-    int nroLinea = 0;
+    int instruccion = 0x00000000;
 
     /*parsed[0] = rotulo, parsed[1] mnemonico SIEMPRE != NULL si no se ignora, parsed[2] y [3] operandos, parsed[4] comment*/
     if (arch != NULL) {
         while (fgets(linea,sizeof linea, arch)!=NULL){
             char **parsed = parseline(linea);
             if (parsed[1]) {
+                instruccion = instruccion | traduceMnemonico(parsed[1]);
             } else {
                 if (!parsed[2] && !parsed[3] && parsed[4]){
                     printf(parsed[4]);
@@ -47,7 +50,6 @@ void leerArchivo(int instructtiones[]){
     } else {
         printf("[ERROR] El archivo no existe");
     }
-    nroLinea++;
 }
 
 /*Esta funcion traduce los mnemonicos de char a binario
@@ -80,6 +82,83 @@ int traduceMnemonico(char instruccion[]){
             return k << 20 | 0xFF000000;
         }
     }
-    return 0xFFFFFFFF;
+    return 0xFFFFFFFF; // mnemonico inexistente
+}
+
+int traduceOperando(int codOp, char operando1[], char operando2[]){
+    /*if (codOp & 0xF0000000 != 0xF0000000) { //operador con dos operandos
+
+    } else if (codOp & 0xFF000000 != 0xFF000000){ //operador con un operando
+
+    } else { //sin operandos
+        return
+    }*/
+}
+
+/*esta funcion determina el tipo de operando y devuelve el numero convertido*/
+int convierteOperando(char operando[]){
+    if (operando[0] == '[') { //OPERADOR DIRECTO
+        char aux[];
+        int k = 0;
+        for(size_t i=1; i < strlen(aux) - 1; i++){ //recorremos entre corchetes
+            aux[k] = operando[i];
+            k++
+        }
+        return parserNumeros(aux);
+    }
+}
+
+int parserNumeros(char num[]){
+    /* la idea es recibir un número en formato string
+    y convertirlo a decimal int basados en la especificación de la MV */
+    char aux[100];
+    int k = 0;
+    if (num[0] == '@'){ //OCTAL
+        for(size_t i=1; i<strlen(num); i++){
+            aux[k] = num[i];
+            k++;
+        }
+        return (int) strtol(aux, NULL, 8);
+    } else if (num[0] == '%'){ //HEXA
+        for(size_t i=1; i<strlen(num); i++){
+            aux[k] = num[i];
+            k++;
+        }
+        return (int) strtol(aux, NULL, 16);
+    } else if (num[0] == '‘'){ //CARACTER ASCII
+        return (int) num[1];
+    } else { //DECIMAL
+        size_t startValue = num[0] == '#' ? 1 : 0;
+        for(size_t i = startValue; i<strlen(num); i++){
+            aux[k] = num[i];
+            k++;
+        }
+        return (int) strtol(aux, NULL, 10);
+    }
+}
+
+/*Devuelve la longitud del code segment, toma en cuenta las lineas
+que poseen mnemonico, ya sea valido o no ya que todos son traducidos igualmente
+Dado que es una longitud, el DS comienza en el valor de longitudCS*/
+
+int calculaCS (char nombreArchivo[]){
+    FILE * arch = fopen(nombreArchivo, "r+");
+    char linea[200];
+    int longitudCS = 0;
+    /*parsed[0] = rotulo, parsed[1] mnemonico SIEMPRE != NULL si no se ignora, parsed[2] y [3] operandos, parsed[4] comment*/
+    if (arch != NULL) {
+        while (fgets(linea,sizeof linea, arch)!=NULL){
+            char **parsed = parseline(linea);
+            if (parsed[1]) {
+                instruccion = instruccion | traduceMnemonico(parsed[1]);
+                cs++;
+            }
+        }
+        fclose(arch);
+    } else {
+        printf("[ERROR] El archivo no existe");
+    }
+
+    return longitudCS;
 }
 
